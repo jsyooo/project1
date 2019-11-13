48d57085406c8837ca678bda8cc72ccb27a72d34
jsyooo <yjs3030@gmail.com>
2019-11-13 12:15:18 +0900

    26일차 예제

---
+++ study/Day26/JS/naver 예제.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.keyboard').click(function(){
+    $('.keyboard-icon').toggleClass('keyboard-on');
+  })
+  $('.auto').click(function(){
+    $('.auto-icon').toggleClass('auto-open');
+  })
+})
