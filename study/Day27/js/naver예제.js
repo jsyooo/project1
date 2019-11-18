b9a55594dfb5445f5ad05ac4561130b4e75fdf23
jsyooo <yjs3030@gmail.com>
2019-11-18 16:16:22 +0900

    29일차

--- study/Day27/js/naver예제.js
+++ study/Day27/js/naver예제.js
@@ -7,8 +7,12 @@ $(function(){
 		$('.auto-box').toggle();
 	})
 	$('.more').click(function(){
-		$(this).toggleClass('fold');
-		$('.more-box').toggle();
+		$(this).toggleClass('fold'); //접기를 더보기로 바꿈
+		$('.more-box').toggle(); //박스 접는 역할
+		$('.white-menu-sub1').removeClass('display-none');
+		$('.white-menu-sub2').addClass('display-none'); //접을때 버튼 초기화
+		$('.black-container').removeClass('display-none');
+		$('.empty-container').addClass('display-none');
 	})
 	$('.r2-btn').click(function(){
 		// 다음 페이지로 가야할지 이전페이지로 가야할지를 결정하는 변수
@@ -45,4 +49,32 @@ $(function(){
 	$('.subs').hover(function(){
 		$('.viewnews').toggleClass('hover-border')
 	})
+	$('.more').click(function(){
+		$('.service-btn').toggle();
+	})
+	$('.sv-close').click(function(){
+		$('.more').click();
+	})
+	$('.menu-setting').click(function(){
+		$('.white-menu-sub1').toggleClass('display-none');
+		$('.white-menu-sub2').toggleClass('display-none');
+		$('.black-container').toggleClass('display-none');
+		$('.empty-container').toggleClass('display-none');
+		// black-box 없어지고 네모박스
+		// 서브메뉴 체크박스	
+	})
+	$('.cancel').click(function(){
+		$('.white-menu-sub2').toggleClass('display-none');
+		$('.white-menu-sub1').toggleClass('display-none');
+		$('.black-container').toggleClass('display-none');
+		$('.empty-container').toggleClass('display-none');
+		// toggle();하면 display이 none <-> block 되므로 flex 해도 지워짐.
+		// 태그의 style 속성으로 작업되므로 우선순위가 매우 높다.
+		// toggle과 toggleClass를 잘못 사용하여 충돌되는 경우,
+		// toggle에 있는 css가 적용된다. -> 원하지 않는 결과 
+	})
+	$('.menu-sub').hover(function(){
+		$(this).toggleClass('hover-underline')
+	})
+
 })
