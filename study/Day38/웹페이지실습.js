4efb2dff25fd7394f0611529e4129f0f5d20b889
jsyooo <yjs3030@gmail.com>
2019-12-04 16:15:38 +0900

    41일차 웹페이지2

--- study/Day38/웹페이지실습.js
+++ study/Day38/웹페이지실습.js
@@ -5,4 +5,25 @@ $(function(){
   $('.btn').hover(function(){
     $(this).toggleClass('btn-slct')
   })
+  $('.photocard').hover(function(){
+    $(this).toggleClass('selcted-photocard')
+    $(this).find('.photo').toggleClass('selcted-photo')
+    $(this).find('.detail').toggleClass('selected-detail')
+    $(this).find('i').toggleClass('selected-down')
+  })
+  $('.traveltype').hover(function(){
+    $(this).find('div').toggleClass('type-slct')
+    $(this).find('i').toggleClass('type-slct')
+  })
+  $('.comment').hover(function(){
+    $(this).find('.active-bar').toggleClass('display-none')
+  })
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 30
+  });
+  var swiper2 = new Swiper('.CommentSlide', {
+    slidesPerView: 3,
+    spaceBetween: 30
+  });
 })
